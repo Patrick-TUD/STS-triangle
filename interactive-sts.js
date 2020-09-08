59358876eccdb5e7f3442a44d16b2e4af36b36a3
patrick.keesmaat <pkeesmaat@tudelft.nl>
2020-09-08 15:42:41 +0200

    Added basis for parameter graphs

--- interactive-sts.js
+++ interactive-sts.js
@@ -5,6 +5,36 @@ let center_width = null;
 let center_height = null;
 let triangle_side_length = 250;
 
+const paramgraphs = [];
+let graph_h_spacing = 50;
+let graph_v_spacing = Math.floor(2*canvas.height/6);
+let parameter_graph_count = 22;
+let parameter_graphs_per_line = parameter_graph_count/2;
+
+const graph_properties = [];
+graph_properties[0] = ["darkolivegreen", "K_GOAL"];
+graph_properties[1] = ["darkolivegreen", "A_PED"];
+graph_properties[2] = ["darkolivegreen", "B_PED"];
+graph_properties[3] = ["darkolivegreen", "A_OBS"];
+graph_properties[4] = ["darkolivegreen", "B_OBS"];
+graph_properties[5] = ["darkolivegreen", "L"];
+graph_properties[6] = ["orange", "HORIZON"];
+graph_properties[7] = ["orange", "TIME_STEP"];
+graph_properties[8] = ["orange", "POLICY_ELECTION_CYCLE"];
+graph_properties[9] = ["orange", "ALPHA"];
+graph_properties[10] = ["orange", "MIN_FOLLOW_DISTANCE"];
+graph_properties[11] = ["purple", "V_MAX"];
+graph_properties[12] = ["steelblue", "PASS_LENGTH"];
+graph_properties[13] = ["steelblue", "PASS_WIDTH"];
+graph_properties[14] = ["steelblue", "PASS_STRENGTH"];
+graph_properties[15] = ["steelblue", "PASS_ANGLE_BIAS"];
+graph_properties[16] = ["steelblue", "PASS_FACING_ANGLE_MARGIN"];
+graph_properties[17] = ["darkred", "CROSS_LENGTH"];
+graph_properties[18] = ["darkred", "CROSS_DEGS"];
+graph_properties[19] = ["darkred", "CROSS_SLOW_STRENGTH"];
+graph_properties[20] = ["darkred", "CROSS_SIDE_STRENGTH"];
+graph_properties[21] = ["darkred", "CROSS_FACING_ANGLE_MARGIN"];
+
 // ########### TOOLBOX FUNCTIONS ################
 function point_in_triangle(px,py,ax,ay,bx,by,cx,cy){
   //credit: http://www.blackpawn.com/texts/pointinpoly/default.html
@@ -43,6 +73,9 @@ function distToSegment(p, v, w) { return Math.sqrt(distToSegmentSquared(p, v, w)
 function distance_to_line(px, py, ax, ay, bx, by){
   return distToSegment({x: px, y: py}, {x: ax, y: ay}, {x: bx, y: by});
 }
+function map( x,  in_min,  in_max,  out_min,  out_max){
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 // ###############################################
 
 
@@ -53,6 +86,16 @@ function resizeCanvas(){
   canvas.height = Math.max(Math.floor(window.innerHeight-200), 500);
   center_width = Math.floor(canvas.width / 2);
   center_height = Math.floor(canvas.height / 2);
+  
+  graph_h_spacing = 50;
+  graph_v_spacing = Math.floor(2*canvas.height/6);
+
+  for (let i=0; i<paramgraphs.length; i++){
+    // console.log("Repositioning.");
+    paramgraphs[i].xpos = Math.floor(canvas.width/2)-40+i*graph_h_spacing - Math.floor(i/parameter_graphs_per_line)*parameter_graphs_per_line*graph_h_spacing;
+    paramgraphs[i].ypos = Math.floor(canvas.height/3) + Math.floor(i/parameter_graphs_per_line)*graph_v_spacing;
+  }
+
   updateCanvas();
 }
 
@@ -60,10 +103,16 @@ window.addEventListener('load', init)
 
 function init(){
   canvas.style.position = 'absolute';
-  canvas.style.top = "100px";
+  canvas.style.top = "110px";
+
+  for (let i = 0; i < parameter_graph_count; i++){
+    paramgraphs.push(new ParamGraph(graph_properties[i][1], 0, 0, graph_properties[i][0]));
+  }
 
   resizeCanvas();
   // drawTriangle();
+
+  // console.log(paramgraphs);
 };
 
 let mouse_x = null;
@@ -115,7 +164,7 @@ function drag(e){
   // ctx.lineCap = 'round';
   
   mouse_x = e.clientX;
-  mouse_y = e.clientY-100;
+  mouse_y = e.clientY-110;
   if (point_in_triangle( mouse_x, mouse_y, ax, ay, bx, by, cx, cy)){
     // console.log(distance_to_line(mouse_x, mouse_y, ax, ay, bx, by));
     technology_dis = distance_to_line(mouse_x, mouse_y, ax, ay, bx, by) / height;
@@ -133,6 +182,7 @@ function updateCanvas(){
   clearCanvas();
   drawTriangle();
   drawSTSGraphs();
+  drawParamGraphs();
 }
 
 // Clear the canvas
@@ -240,6 +290,46 @@ function drawSTSGraphs(){
   ctx.fillRect(technology_center-bar_width/2, base_height-30-bar_height*technology_dis, bar_width, bar_height*technology_dis);
   ctx.fillStyle = "red";
   ctx.fillRect(service_center-bar_width/2, base_height-30-bar_height*service_dis, bar_width, bar_height*service_dis);
+}
+
+function ParamGraph(name, xpos, ypos, color){
+  this.name = name;
+  this.xpos = xpos;
+  this.ypos = ypos;
+  this.color = color
+  this.bar_width = 20;
+  this.bar_height = 100;
+  this.value = 0.500;
+
+  this.draw = function(){
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.xpos-this.bar_width/2, this.ypos-this.value*this.bar_height, this.bar_width, this.bar_height*this.value);
+    ctx.save();
+    ctx.translate(this.xpos+5, this.ypos+15);
+    let angle_div = map(Math.min(canvas.height, 800), 500, 800, 10, 3);
+    ctx.rotate(-Math.PI/angle_div);  // 10 at min height, 3 at max height
+    ctx.textAlign = "right";
+    // ctx.fillStyle = "blue";
+    ctx.fillText(this.name, 0, 0);
+    ctx.restore();
+  }
+
+  this.setPos = function(xpos, ypos){
+    this.xpos = xpos;
+    this.ypos = ypos;
+  }
+}
+
+function drawParamGraphs() {
+  for (let i = 0; i < paramgraphs.length; i++){
+    // console.log(i + " " + paramgraphs[i].value);
+    paramgraphs[i].value = Math.random();
+    // console.log(i + " " + paramgraphs[i].value);
+    // console.log(i + " " + paramgraphs[i].color);
+    // paramgraphs[i].color = "blue";
+    // console.log(i + " " + paramgraphs[i].color);
+    paramgraphs[i].draw();
+  }
 }
 
 // EventListeners
