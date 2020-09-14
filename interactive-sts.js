bc4f0310d68e54b65d53849eaa78947dd18c4d34
patrick.keesmaat <pkeesmaat@tudelft.nl>
2020-09-14 09:46:12 +0200

    Added parameter description, highlight on hover and fixed triangle marker tracking outside triangle.

--- interactive-sts.js
+++ interactive-sts.js
@@ -12,30 +12,35 @@ let graph_v_spacing = Math.floor(2*canvas.height/6);
 let parameter_graph_count = 22;
 let parameter_graphs_per_line = parameter_graph_count/2;
 
+let parameter_description = "";
+let parameter_description_col = "black";
+let parameter_description_xpos = center_width;
+let parameter_description_ypos = 60;
+
 const graph_properties = [];
-// [0; color, 1; name, 2; base value, 3; social factor, 4; technology factor, 5; service factor]
-graph_properties[0] = ["darkolivegreen", "K_GOAL", 0.4, 0, 0.3, 0.3];
-graph_properties[1] = ["darkolivegreen", "A_PED", 0, 0.8, 0.2, 0];
-graph_properties[2] = ["darkolivegreen", "B_PED", 0, 0.6, 0.3, 0.1];
-graph_properties[3] = ["darkolivegreen", "A_OBS", 0.5, -0.4, 0.2, 0.3];
-graph_properties[4] = ["darkolivegreen", "B_OBS", 0.5, -0.4, 0.3, 0.2];
-graph_properties[5] = ["darkolivegreen", "L", 0.2, 0.8, -0.1, -0.1];
-graph_properties[6] = ["orange", "HORIZON", 0.5, 0.5, -0.1, -0.1];
-graph_properties[7] = ["orange", "TIME_STEP", 0.5, 0.5, -0.3, -0.2];
-graph_properties[8] = ["orange", "POLICY_ELECTION_CYCLE", 1, -0.1, -0.8, -0.8];
-graph_properties[9] = ["orange", "ALPHA", 0.5, -0.5, 0.5, 0];
-graph_properties[10] = ["orange", "MIN_FOLLOW_DISTANCE", 0.4, 0.6, 0, 0];
-graph_properties[11] = ["purple", "V_MAX", 0.6, -0.3, -0.2, 0.4];
-graph_properties[12] = ["steelblue", "PASS_LENGTH", 0, 0.8, 0.1, 0.1];
-graph_properties[13] = ["steelblue", "PASS_WIDTH", 0.2, 0.6, 0.1, 0.1];
-graph_properties[14] = ["steelblue", "PASS_STRENGTH", 0.3, 0.6, 0.1, 0.1];
-graph_properties[15] = ["steelblue", "PASS_ANGLE_BIAS", 0.5, 0.4, 0.1, 0.1];
-graph_properties[16] = ["steelblue", "PASS_FACING_ANGLE_MARGIN", 0.7, 0.3, -0.2, -0.2];
-graph_properties[17] = ["darkred", "CROSS_LENGTH", 0.3, 0.7, -0.3, -0.3];
-graph_properties[18] = ["darkred", "CROSS_DEGS", 0.5, 0.5, -0.2, 0,2];
-graph_properties[19] = ["darkred", "CROSS_SLOW_STRENGTH", 0.6, 0.4, -0.4, -0.4];
-graph_properties[20] = ["darkred", "CROSS_SIDE_STRENGTH", 0.7, 0.3, -0.2, -0.2];
-graph_properties[21] = ["darkred", "CROSS_FACING_ANGLE_MARGIN", 0.7, 0.3, -0.1, -0.1];
+// [0; color, 1; name, 2; base value, 3; social factor, 4; technology factor, 5; service factor, description]
+graph_properties[0] = ["darkolivegreen", "K_GOAL", 0.4, 0, 0.3, 0.3, "Goal force strength factor."];
+graph_properties[1] = ["darkolivegreen", "A_PED", 0, 0.8, 0.2, 0, "Pedestrian force strength factor."];
+graph_properties[2] = ["darkolivegreen", "B_PED", 0, 0.6, 0.3, 0.1, "Pedestrian force range factor."];
+graph_properties[3] = ["darkolivegreen", "A_OBS", 0.5, -0.4, 0.2, 0.3, "Obstacle force strength factor."];
+graph_properties[4] = ["darkolivegreen", "B_OBS", 0.5, -0.4, 0.3, 0.2, "Obstacle force range factor."];
+graph_properties[5] = ["darkolivegreen", "L", 0.2, 0.8, -0.1, -0.1, "Lambda; strength of the anisotropic factor."];
+graph_properties[6] = ["orange", "HORIZON", 0.5, 0.5, -0.1, -0.1, "Length of the forward simulation horizon."];
+graph_properties[7] = ["orange", "TIME_STEP", 0.5, 0.5, -0.3, -0.2, "Number of seconds during each iteration in the horizon."];
+graph_properties[8] = ["orange", "POLICY_ELECTION_CYCLE", 1, -0.1, -0.8, -0.8, "Select new policy every # TIME_STEPS."];
+graph_properties[9] = ["orange", "ALPHA", 0.5, -0.5, 0.5, 0, "The 'progress' weight factor for the MPDM cost function."];
+graph_properties[10] = ["orange", "MIN_FOLLOW_DISTANCE", 0.4, 0.6, 0, 0, "Minimum distance to the leader during the follow policy."];
+graph_properties[11] = ["purple", "V_MAX", 0.6, -0.3, -0.2, 0.4, "Maximum velocity of the robot."];
+graph_properties[12] = ["steelblue", "PASS_LENGTH", 0, 0.8, 0.1, 0.1, "Length of the sensing area for frontal passing."];
+graph_properties[13] = ["steelblue", "PASS_WIDTH", 0.2, 0.6, 0.1, 0.1, "Width of the sensing area for frontal passing."];
+graph_properties[14] = ["steelblue", "PASS_STRENGTH", 0.3, 0.6, 0.1, 0.1, "Passing force strength factor."];
+graph_properties[15] = ["steelblue", "PASS_ANGLE_BIAS", 0.5, 0.4, 0.1, 0.1, "Right hand rule bias in passing."];
+graph_properties[16] = ["steelblue", "PASS_FACING_ANGLE_MARGIN", 0.7, 0.3, -0.2, -0.2, "Angle the other agent's 'look' can deviate for frontal passing."];
+graph_properties[17] = ["darkred", "CROSS_LENGTH", 0.3, 0.7, -0.3, -0.3, "Length of the sensing cone for crossing."];
+graph_properties[18] = ["darkred", "CROSS_DEGS", 0.5, 0.5, -0.2, 0.2, "Angle of the sensing cone, centered on robots heading."];
+graph_properties[19] = ["darkred", "CROSS_SLOW_STRENGTH", 0.6, 0.4, -0.4, -0.4, "Speed reduction force factor for pedestrian crossing."];
+graph_properties[20] = ["darkred", "CROSS_SIDE_STRENGTH", 0.7, 0.3, -0.2, -0.2, "Side stepping force factor for pedestrian crossing."];
+graph_properties[21] = ["darkred", "CROSS_FACING_ANGLE_MARGIN", 0.7, 0.3, -0.1, -0.1, "Angle the other agent's 'look' can deviate for pedestrian crossing."];
 
 // ########### TOOLBOX FUNCTIONS ################
 function point_in_triangle(px,py,ax,ay,bx,by,cx,cy){
@@ -88,7 +93,7 @@ function distance_to_line(px, py, ax, ay, bx, by){
 function map( x,  in_min,  in_max,  out_min,  out_max){
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-// ###############################################
+// ########### END OF TOOLBOX FUNCTIONS ################
 
 
 
@@ -103,10 +108,12 @@ function resizeCanvas(){
   graph_v_spacing = Math.floor(2*canvas.height/6);
 
   for (let i=0; i<paramgraphs.length; i++){
-    // console.log("Repositioning.");
     paramgraphs[i].xpos = Math.floor(canvas.width/2)-40+i*graph_h_spacing - Math.floor(i/parameter_graphs_per_line)*parameter_graphs_per_line*graph_h_spacing;
     paramgraphs[i].ypos = Math.floor(canvas.height/3) + Math.floor(i/parameter_graphs_per_line)*graph_v_spacing;
   }
+
+  parameter_description_xpos = Math.floor(canvas.width/2)-40+Math.floor(parameter_graphs_per_line/2) * graph_h_spacing;
+  parameter_description_ypos = Math.floor(canvas.height/3)-120;
 
   updateCanvas();
 }
@@ -118,17 +125,16 @@ function init(){
   canvas.style.top = `${canvas_top}px`;
 
   for (let i = 0; i < parameter_graph_count; i++){
-    paramgraphs.push(new ParamGraph(graph_properties[i][1], 0, 0, graph_properties[i][0], graph_properties[i][2], graph_properties[i][3], graph_properties[i][4], graph_properties[i][5]));
+    paramgraphs.push(new ParamGraph(graph_properties[i][1], 0, 0, graph_properties[i][0], graph_properties[i][2], graph_properties[i][3], graph_properties[i][4], graph_properties[i][5], graph_properties[i][6]));
   }
 
   resizeCanvas();
-  // drawTriangle();
-
-  // console.log(paramgraphs);
 };
 
 let mouse_x = null;
 let mouse_y = null;
+let marker_x = null;
+let marker_y = null;
 let height = null;
 let triangle_center = null;
 let triangle_base_height = null;
@@ -171,24 +177,51 @@ function finishedPosition(){
 }
 
 function drag(e){
-  if(!dragging) return;
-  // ctx.lineWidth = 5;
-  // ctx.lineCap = 'round';
-  
-  mouse_x = e.clientX;
-  mouse_y = e.clientY-canvas_top;
-  if (point_in_triangle( mouse_x, mouse_y, ax, ay, bx, by, cx, cy)){
-    // console.log(distance_to_line(mouse_x, mouse_y, ax, ay, bx, by));
-    technology_dis = distance_to_line(mouse_x, mouse_y, ax, ay, bx, by) / height;
-    social_dis = distance_to_line(mouse_x, mouse_y, cx, cy, bx, by) / height;
-    service_dis = distance_to_line(mouse_x, mouse_y, ax, ay, cx, cy) / height;
+  if(!dragging){
+    // Check if mouse is hovering over an parameter, if so provide a description, otherwise clear the description.
+    let mouse_x_info = e.clientX;
+    let mouse_y_info = e.clientY-canvas_top;
+    let flag = false;
+    for (let i=0; i<paramgraphs.length; i++){
+      // 1. Check if mouse is within bounding box.
+      if (paramgraphs[i].pointInBoundingBox(mouse_x_info, mouse_y_info)){
+        // console.log(i);
+        // 2. Set descriptive text to own description.
+        parameter_description = paramgraphs[i].description;
+        parameter_description_col = paramgraphs[i].color;
+        // 3. if found, return.
+        flag = true;
+      }
+    }
+    // 4. No return? Clear the description.
+    if (!flag){parameter_description = ""};
+  }else{
+    // Update mouse position in triangle and calculate new aspect and parameter values. 
+    mouse_x = e.clientX;
+    mouse_y = e.clientY-canvas_top;
+    if (point_in_triangle( mouse_x, mouse_y, ax, ay, bx, by, cx, cy)){
+      // console.log(distance_to_line(mouse_x, mouse_y, ax, ay, bx, by));
+      // Point within triangle, marker position is mouse position;
+      marker_x = mouse_x;
+      marker_y = mouse_y;
+    }else{
+      // Point outside triangle, marker position is closest point to mouse position while within triangle.
+      let marker = pointOnLine({x: mouse_x, y: mouse_y}, {x: ax, y: ay}, {x: bx, y: by});
+      if (dist2(marker, {x: mouse_x, y: mouse_y}) > dist2(pointOnLine({x: mouse_x, y: mouse_y}, {x: bx, y: by}, {x: cx, y: cy}), {x: mouse_x, y: mouse_y})){
+        marker = pointOnLine({x: mouse_x, y: mouse_y}, {x: bx, y: by}, {x: cx, y: cy});
+      }
+      if (dist2(marker, {x: mouse_x, y: mouse_y}) > dist2(pointOnLine({x: mouse_x, y: mouse_y}, {x: ax, y: ay}, {x: cx, y: cy}), {x: mouse_x, y: mouse_y})){
+        marker = pointOnLine({x: mouse_x, y: mouse_y}, {x: ax, y: ay}, {x: cx, y: cy});
+      }
+      marker_x = marker.x;
+      marker_y = marker.y;
+    }
+    technology_dis = distance_to_line(marker_x, marker_y, ax, ay, bx, by) / height;
+    social_dis = distance_to_line(marker_x, marker_y, cx, cy, bx, by) / height;
+    service_dis = distance_to_line(marker_x, marker_y, ax, ay, cx, cy) / height;
     updateParamValues();
-    updateCanvas();
-    ctx.strokeStyle = '#888888';
-    ctx.arc(mouse_x, mouse_y, 5, 0, Math.PI * 2, false);
-    ctx.lineWidth = 2;
-    ctx.stroke();
   }
+  updateCanvas();
 }
 
 // Update canvas
@@ -197,6 +230,7 @@ function updateCanvas(){
   drawTriangle();
   drawSTSGraphs();
   drawParamGraphs();
+  drawParameterDescription();
 }
 
 // Clear the canvas
@@ -274,27 +308,33 @@ function drawTriangle(){
   ctx.lineTo(bx, by);
   ctx.stroke();
 
-  //Thin colored lines from input point to aspect edges.
-  if (mouse_x != null){
+  //Thin colored lines from input point to aspect edges and circle marker.
+  if (marker_x != null){
     ctx.beginPath();
-    ctx.moveTo(mouse_x, mouse_y);
-    var p_service = pointOnLine({x: mouse_x, y: mouse_y}, {x: ax, y: ay}, {x: cx, y: cy});
+    ctx.moveTo(marker_x, marker_y);
+    var p_service = pointOnLine({x: marker_x, y: marker_y}, {x: ax, y: ay}, {x: cx, y: cy});
     ctx.lineTo(p_service.x, p_service.y);
     ctx.strokeStyle = "red";
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.moveTo(mouse_x, mouse_y);
-    var p_social = pointOnLine({x: mouse_x, y: mouse_y}, {x: bx, y: by}, {x: cx, y: cy});
+    ctx.moveTo(marker_x, marker_y);
+    var p_social = pointOnLine({x: marker_x, y: marker_y}, {x: bx, y: by}, {x: cx, y: cy});
     ctx.lineTo(p_social.x, p_social.y);
     ctx.strokeStyle = "green";
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.moveTo(mouse_x, mouse_y);
-    var p_technology = pointOnLine({x: mouse_x, y: mouse_y}, {x: bx, y: by}, {x: ax, y: ay});
+    ctx.moveTo(marker_x, marker_y);
+    var p_technology = pointOnLine({x: marker_x, y: marker_y}, {x: bx, y: by}, {x: ax, y: ay});
     ctx.lineTo(p_technology.x, p_technology.y);
     ctx.strokeStyle = "blue";
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle = '#888888';
+    ctx.arc(marker_x, marker_y, 5, 0, Math.PI * 2, false);
+    ctx.lineWidth = 2;
     ctx.stroke();
   }
 
@@ -341,7 +381,7 @@ function drawSTSGraphs(){
 
 }
 
-function ParamGraph(name, xpos, ypos, color, base, social_val, technology_val, service_val){
+function ParamGraph(name, xpos, ypos, color, base, social_val, technology_val, service_val, description){
   this.name = name;
   this.xpos = xpos;
   this.ypos = ypos;
@@ -353,8 +393,20 @@ function ParamGraph(name, xpos, ypos, color, base, social_val, technology_val, s
   this.social_val = social_val;
   this.technology_val = technology_val;
   this.service_val = service_val;
+  this.description = description;
+  this.bounding_width = 25;
+  this.bounding_height = this.bar_height*1.5;
+  this.on_hover = false;
 
   this.draw = function(){
+    // On hover debug
+    if (this.on_hover){
+      // let background_alpha = 0.5;
+      ctx.fillStyle = "white";//`rgba(255, 255, 255, ${background_alpha})`;
+      ctx.fillRect(this.xpos-this.bounding_width, this.ypos-this.bar_height-15, this.bounding_width*2, this.bounding_height+10);
+    }
+
+    // Param graph and info itself.
     ctx.font = '13px "Arial"';
     ctx.fillStyle = this.color;
     ctx.globalAlpha = 0.3;
@@ -379,6 +431,11 @@ function ParamGraph(name, xpos, ypos, color, base, social_val, technology_val, s
   this.updateValue = function(){
     this.value = this.base + social_dis*this.social_val + technology_dis*this.technology_val + service_dis*this.service_val;
   }
+
+  this.pointInBoundingBox = function(point_x, point_y){
+    this.on_hover = (point_x >= this.xpos-this.bounding_width && point_x < this.xpos+this.bounding_width && point_y >= this.ypos-this.bar_height-15 && point_y <= this.ypos-this.bar_height-5+this.bounding_height);
+    return this.on_hover;
+  }
 }
 
 function drawParamGraphs() {
@@ -398,6 +455,15 @@ function updateParamValues(){
   for (let i = 0; i < paramgraphs.length; i++){
     paramgraphs[i].updateValue();
   }
+}
+
+function drawParameterDescription() {
+  ctx.font = '16px "Arial"';
+  ctx.fillStyle = parameter_description_col;
+  ctx.textBaseline = "bottom";
+  ctx.textAlign = "center";
+  ctx.fillText(parameter_description, parameter_description_xpos, parameter_description_ypos);
+  // ctx.fillText("parameter_description", center_width, 30);
 }
 
 // EventListeners
